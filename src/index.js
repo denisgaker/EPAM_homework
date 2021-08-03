b1725ee5bae9ca676d7744cf6ba0613ec3e3c63c
denisgaker <denisgaker@gmail.com>
2021-08-03 11:49:01 +0300

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  btn.innerHTML = 'click me';
+  btn.onclick = printMe;
+
   element.innerHTML = _.join(['Hello', 'webpack']);
   element.classList.add('hello');
   
@@ -11,6 +17,7 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
---
+++ src/print.js
@@ -0,0 +1,3 @@
+export default function printMe() {
+    console.log('I get callod form print.js');
+}
