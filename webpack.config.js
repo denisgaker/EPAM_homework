48d4d2675c8492631d9a3f16d9769638020f82fc
denisgaker <denisgaker@gmail.com>
2021-08-03 12:49:07 +0300

    Environment Variables (вариант)

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['@babel/plugin-transform-runtime']
+module.exports = (env) => {
+    console.log('Goal: ', env.goal); //local
+    console.log('Production: ', env.prodaction);
+    return {
+        mode: 'development',
+        entry: './src/index.js',
+        devServer: {
+            contentBase: './dist'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Output Management'
+            })
+        ],
+        output: {
+            filename: 'bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-transform-runtime']
+                        }
                     }
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|svg|jpeg|jpg|gif)$/i,
+                    type: 'asset/resource'
                 }
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpeg|jpg|gif)$/i,
-                type: 'asset/resource'
-            }
-        ]
+            ]
+        }
     }
 }
