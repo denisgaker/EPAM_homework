93384eeb970289c872ef135b6ec7e73d88df137d
denisgaker <denisgaker@gmail.com>
2021-08-03 11:03:42 +0300

    babel + npm command

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,20 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    }
+                }
+            }
+        ]
     }
 }
