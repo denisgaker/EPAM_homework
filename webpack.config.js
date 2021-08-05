a8180a2547c09d957fcaabf56749f75430f78272
denisgaker <denisgaker@gmail.com>
2021-08-05 19:32:07 +0300

    Have dev and prod build commands(use evn variables)

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+    console.log('Goal: ', env.goal, '\nProduction: ', env.production, '\nmySecretKey: ', env.mySecretKey);
+    return {
+        mode: env.production ? 'production' : 'development',
+        devtool: env.production ? false : 'inline-source-map',
+        entry: './src/index.js',
+        devServer: env.production ? { contentBase: false } : { contentBase: './dist' },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Output Management'
+            })
+        ],
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.m?(js)$/,
+                    exclude: /(node_modules|bower_components)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                            plugins: ['@babel/plugin-transform-runtime']
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/i,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.(png|svg|jpeg|jpg|gif)$/i,
+                    type: 'asset/resource'
+                }
+            ]
+        }
+    }
+}
