4d56ab481f0ca0f2ad42fb331766dea36a01682c
denisgaker <denisgaker@gmail.com>
2021-08-07 19:21:50 +0300

    Разметка и стили для шапки сайта

---
+++ src/header/header/Header.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import '../style.css';
+
+const Header: React.FC<{}> = (props) => (
+    <header className="header">{props.children}</header>
+);
+
+export default Header;
---
+++ src/header/header/Heading.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { HeadingProps } from "./Header.interface";
+
+const Heading: React.FC<HeadingProps> = ({ name }: HeadingProps) => {
+    return <h1>{`${name}`}</h1>;
+}
+
+export default Heading;
