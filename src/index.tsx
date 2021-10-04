b847a376ab4a87e312ce3302b98775f0305768d5
denisgaker <denisgaker@gmail.com>
2021-10-04 08:53:11 +0300

    Создал combineReducer и 'прокинул' его в store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
 import App from './App';
+// import { createStore } from 'redux';
 
 const appContainer: HTMLElement = document.createElement('section');
 appContainer.id = 'app';
