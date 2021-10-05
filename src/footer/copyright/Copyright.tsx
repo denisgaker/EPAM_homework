e147bfeb7bdba93d31e3f642deb03a78055c563e
denisgaker <denisgaker@gmail.com>
2021-10-05 14:32:26 +0300

    Удалил console.log

--- src/footer/copyright/Copyright.tsx
+++ src/footer/copyright/Copyright.tsx
@@ -3,7 +3,6 @@ import { copyType } from './Copyright.interface';
 
 const Copyright: React.FC<copyType> = ({ name }: copyType) => {
   const date = new Date().getFullYear();
-  console.log('date: ', date);
   const text = ' | All right reserved';
   return <p>&#169;{` ${date} ${name} ${text}`}</p>;
 };
