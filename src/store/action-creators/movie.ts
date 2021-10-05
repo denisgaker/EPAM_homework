d61d23997960d452d7ad22444045e720733db770
denisgaker <denisgaker@gmail.com>
2021-10-05 12:02:52 +0300

    Расширили actionCreator (добавил параметры и actionCreator (setMoviePage) для пагинации

--- src/store/action-creators/movie.ts
+++ src/store/action-creators/movie.ts
@@ -2,11 +2,13 @@ import { Dispatch } from 'react';
 import axios from 'axios';
 import { MovieAction, MoviesActionTypes } from '../reducers/moviesReducer.interface';
 
-export const fetchMovies = () => {
+export const fetchMovies = (page = 1, limit = 9) => {
   return async (dispatch: Dispatch<MovieAction>): Promise<void> => {
     try {
       dispatch({ type: MoviesActionTypes.FETCH_MOVIES });
-      const response = await axios.get('https://reactjs-cdp.herokuapp.com/movies');
+      const response = await axios.get('https://reactjs-cdp.herokuapp.com/movies', {
+        params: { page: page, limit: limit },
+      });
       console.log('response: ', response.data);
       dispatch({ type: MoviesActionTypes.FETCH_MOVIES_SUCCESS, payload: response.data });
     } catch (e) {
@@ -16,4 +18,8 @@ export const fetchMovies = () => {
       });
     }
   };
+};
+
+export const setMoviePage = (page: number): MovieAction => {
+  return { type: MoviesActionTypes.SET_MOVIES_PAGE, payload: page };
 };
