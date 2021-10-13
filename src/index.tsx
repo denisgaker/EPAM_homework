784647fcf6855c6c495014a628cadaf8941c1ceb
denisgaker <denisgaker@gmail.com>
2021-10-13 18:16:17 +0300

    Offline redux-persist (промежуточный результат)

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './store';
-// import { createStore } from 'redux';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const appContainer: HTMLElement = document.createElement('section');
 appContainer.id = 'app';
@@ -11,9 +11,11 @@ document.body.appendChild(appContainer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>,
   appContainer
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,21 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './reducers';
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'rootReducer',
+  storage: storage,
+};
+
+const pReducer = persistReducer(persistConfig, rootReducer);
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(pReducer, middleware);
+const persistor = persistStore(store);
+
+export { persistor, store };
+
+// ! v1
+// export const store = createStore(rootReducer, applyMiddleware(thunk));
