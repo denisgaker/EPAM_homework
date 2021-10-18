dbc3e83e7f8eeb46fe2139c0c44bf43e9b2490c7
denisgaker <denisgaker@gmail.com>
2021-10-18 15:42:39 +0300

    Сделал компонент для стартовой страницы с заголовком '0'

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,41 @@ import { Datats } from './Data';
 import Header from './header/header/Header';
 import Heading from './header/header/Heading';
 import Search from './header/search/Search';
-import MovieList2 from './content/MovieList2';
+// import MovieList2 from './content/MovieList2';
 // import MovieList from './content/MovieList';
 // import ErrorBoundary from './errorBoundary/ErrorBoundary';
 import Footer from './footer/footer/Footer';
 import SocLinks from './footer/SocLinks';
 import './style.css';
 import Copyright from './footer/copyright/Copyright';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Default from './pages/Default';
 
 const NameApp = Datats.main.nameApp;
 const SearchCTA = Datats.main.searchCTA;
 const SearchPlaceholder = Datats.main.searchPlaceholder;
 const SearchBtnText = Datats.main.searchBtnText;
 
-const App = (): React.ReactElement => {
-  return (
-    <>
-      <Header>
-        <Heading name={NameApp} />
-        <Search
-          searchCTA={SearchCTA}
-          searchPlaceholder={SearchPlaceholder}
-          searchBtnText={SearchBtnText}
-        />
-      </Header>
-      {/* <ErrorBoundary> */}
-      <MovieList2 />
-      {/* </ErrorBoundary> */}
-      <Footer>
-        <Copyright name={NameApp} />
-        <SocLinks />
-      </Footer>
-    </>
-  );
-};
+const App = (): React.ReactElement => (
+  <>
+    <Header>
+      <Heading name={NameApp} />
+      <Search
+        searchCTA={SearchCTA}
+        searchPlaceholder={SearchPlaceholder}
+        searchBtnText={SearchBtnText}
+      />
+    </Header>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Default} />
+      </Switch>
+    </BrowserRouter>
+    <Footer>
+      <Copyright name={NameApp} />
+      <SocLinks />
+    </Footer>
+  </>
+);
 
 export default App;
---
+++ src/pages/Default.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const Default = (): React.ReactElement => {
+  return <h1 style={{ textAlign: 'center', width: '100%' }}>0</h1>;
+};
+
+export default Default;
