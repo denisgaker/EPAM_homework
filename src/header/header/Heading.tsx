e1793d70c244c8289fb36c18cdd3d4fcf22908b7
denisgaker <denisgaker@gmail.com>
2021-10-18 15:51:46 +0300

    Обернул заголовок в Header в NavLink, чтобы по клику на него происходил переход на главную страницу

--- src/header/header/Heading.tsx
+++ src/header/header/Heading.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { HeadingProps } from './Header.interface';
+import { NavLink } from 'react-router-dom';
 
 const Heading: React.FC<HeadingProps> = ({ name }: HeadingProps) => {
-  return <h1>{`${name}`}</h1>;
+  return (
+    <h1>
+      <NavLink to={'/'}>{`${name}`}</NavLink>
+    </h1>
+  );
 };
 
 export default Heading;
