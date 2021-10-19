164f62b97b76a32a3a6f93aff341c931997cb740
denisgaker <denisgaker@gmail.com>
2021-10-19 14:03:20 +0300

    Переименовал компонент для стартовой страницы

---
+++ src/pages/DefaultPage.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const DefaultPage = (): React.ReactElement => {
+  return <h1 style={{ textAlign: 'center', width: '100%' }}>0</h1>;
+};
+
+export default DefaultPage;
