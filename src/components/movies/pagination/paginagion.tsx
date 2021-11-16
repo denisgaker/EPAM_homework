e8700f93084d4c2dcc30d022bb4efec53d5bd765
denisgaker <denisgaker@gmail.com>
2021-11-16 18:46:59 +0300

    Пагинация (в процессе)

--- src/components/movies/pagination/paginagion.tsx
+++ src/components/movies/pagination/paginagion.tsx
@@ -4,7 +4,6 @@ import { useLocation } from 'react-router-dom';
 import Url from 'url-parse';
 import useActions from '../../../hooks/useActions';
 import useTypeSelector from '../../../hooks/useTypeSelector';
-// import pages from '../movieList.constants';
 import styles from './style';
 
 interface StyledPaginationProps extends WithStylesProps<typeof styles> {}
@@ -12,12 +11,12 @@ interface StyledPaginationProps extends WithStylesProps<typeof styles> {}
 const Pagination:FC<StyledPaginationProps> = ({ classes }) => {
   const { limit, total } = useTypeSelector((state) => state.movies);
   const { fetchMovies } = useActions();
+
   const { search } = useLocation();
   const urlParams = new Url(search, true);
-  const pages = Array.from({ length: total / 9 }, (v, i) => i + 1);
-  /* console.log('Делим общее кол-во на 9: ', total / 9);
-  console.log('Получаем остаток от деления на 9: ', total % 9); */
-  // console.log('Получаем количество страниц: ', Math.ceil(total / 9));
+
+  const pages = Array.from({ length: Math.ceil(total / 9) }, (v, i) => i + 1);
+
   return (
     <div className={classes.pagination}>
         {pages.map((pageNum) => (
